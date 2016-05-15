833441900265994a5406b9fa65c2f45fdc5f5514
jorgeeandradec <jorgejecp@hotmail.com>
2016-05-15 18:58:29 -0500

    Creación de botón para imprimir y mejoras en la programación para escalar

--- js/recibirDatos.js
+++ js/recibirDatos.js
@@ -1,54 +1,59 @@
-var canvas, contexto;
+var canvas, contexto, botonImprimir;
 
 var frase = "SENA Virtual";
 
-var anchoCelda = 100;
-var altoCelda = 20;
+var filas = 26;
+var columnas = 9;
+var anchoCalendario = 800;
+var altoCalendario = 500;
+var anchoCelda = anchoCalendario/(columnas-1);
+var altoCelda = altoCalendario/(filas-1);
+
 
 var celdaLunes = {
-	x:100,
-	ancho:100,
+	x:anchoCelda*1,
+	ancho:anchoCelda,
 	nombre: "Lunes"
 	//al definir atributos del objero se debe dar valor a cada uno aquí
 	//si no, entonces se van creando en la programación
 };
 
 var celdaMartes = {
-	x:200,
-	ancho:100,
+	x:anchoCelda*2,
+	ancho:anchoCelda,
 	nombre: "Martes"
 }
 
 var celdaMiercoles = {
-	x:300,
-	ancho:100,
+	x:anchoCelda*3,
+	ancho:anchoCelda,
 	nombre: "Miercoles"
 }
 
 var celdaJueves = {
-	x:400,
-	ancho:100,
+	x:anchoCelda*4,
+	ancho:anchoCelda,
 	nombre: "Jueves"
 }
 
 var celdaViernes = {
-	x:500,
-	ancho:100,
+	x:anchoCelda*5,
+	ancho:anchoCelda,
 	nombre: "Viernes"
 }
 
 var celdaSabado = {
-	x:600,
-	ancho:100,
+	x:anchoCelda*6,
+	ancho:anchoCelda,
 	nombre: "Sabado"
 }
 var celdaDomingo = {
-	x:700,
-	ancho:100,
+	x:anchoCelda*7,
+	ancho:anchoCelda,
 	nombre: "Domingo"
 }
 
-var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo", ""];
 var horas = ["01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00",
  "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00",
  "20:00", "21:00", "22:00", "23:00", "24:00" ];
@@ -126,27 +131,27 @@ if(params['documento']){
 //Definir las caracteristicas de las Celdas de cada día para poder rellenar en calendario
 
 
-	celdaLunes.y = inicioLunes * 20;
-	celdaLunes.alto = (finLunes - inicioLunes) * 20;
+	celdaLunes.y = inicioLunes * altoCelda;
+	celdaLunes.alto = (finLunes - inicioLunes) * altoCelda;
 	celdaLunes.horasDia = finLunes - inicioLunes;
 
-	celdaMartes.y = inicioMartes * 20;
-	celdaMartes.alto = (finMartes - inicioMartes) * 20;
+	celdaMartes.y = inicioMartes * altoCelda;
+	celdaMartes.alto = (finMartes - inicioMartes) * altoCelda;
 
-	celdaMiercoles.y = inicioMiercoles * 20;
-	celdaMiercoles.alto = (finMiercoles - inicioMiercoles) * 20;
+	celdaMiercoles.y = inicioMiercoles * altoCelda;
+	celdaMiercoles.alto = (finMiercoles - inicioMiercoles) * altoCelda;
 
-	celdaJueves.y = inicioJueves * 20;
-	celdaJueves.alto = (finJueves - inicioJueves) * 20;
+	celdaJueves.y = inicioJueves * altoCelda;
+	celdaJueves.alto = (finJueves - inicioJueves) * altoCelda;
 
-	celdaViernes.y = inicioViernes * 20;
-	celdaViernes.alto = (finViernes - inicioViernes) * 20;
+	celdaViernes.y = inicioViernes * altoCelda;
+	celdaViernes.alto = (finViernes - inicioViernes) * altoCelda;
 
-	celdaSabado.y = inicioSabado * 20;
-	celdaSabado.alto = (finSabado - inicioSabado) * 20;
+	celdaSabado.y = inicioSabado * altoCelda;
+	celdaSabado.alto = (finSabado - inicioSabado) * altoCelda;
 
-	celdaDomingo.y = inicioDomingo * 20;
-	celdaDomingo.alto = (finDomingo - inicioDomingo) * 20;
+	celdaDomingo.y = inicioDomingo * altoCelda;
+	celdaDomingo.alto = (finDomingo - inicioDomingo) * altoCelda;
 
 	document.write(tipoDocumento);
 	document.write(documento);
@@ -176,21 +181,24 @@ compararHora(celdaLunes.nombre, celdaLunes.horasDia);
 
 canvas = document.getElementById("grilla");
 contexto = canvas.getContext("2d");
+botonImprimir = document.getElementById("botonImprimir");
+botonImprimir.addEventListener("click", imprimirCalendario);
+
+
 
 
 //Dibujando filas
-for(i=1; i < 26; i++){
+
+for(i=1; i < filas; i++){
 	dibujarCeldas(0, i);
 }
 
 //Dibujando Columnas
-for(i=1; i <= 9; i++){
+ 
+for(i=1; i <= columnas; i++){
 	dibujarCeldas(i, 0);
 
 }
-
-
-
 //Definiendo el color de las celdas a través de la función definirCelda
 definirCelda(celdaLunes.x, celdaLunes.y, celdaLunes.ancho, celdaLunes.alto);
 definirCelda(celdaMartes.x, celdaMartes.y, celdaMartes.ancho, celdaMartes.alto);
@@ -205,17 +213,25 @@ definirCelda(celdaDomingo.x, celdaDomingo.y, celdaDomingo.ancho, celdaDomingo.al
 
 }
 
-function dibujarCeldas(x, y){			
+function imprimirCalendario(){
+		window.print();	
+	}
+
+
+
+function dibujarCeldas(x, y){
 
 			
-			contexto.moveTo(0, y * 20);
-			contexto.lineTo(800, y * 20);
+
+			
+			contexto.moveTo(0, y * altoCelda);
+			contexto.lineTo(anchoCalendario, y * altoCelda);
 			contexto.stroke();
 			contexto.strokeStyle = "#cc0000";
 			
 
-			contexto.moveTo(x * 100, 0);
-			contexto.lineTo(x * 100, 500);
+			contexto.moveTo(x * anchoCelda, 0);
+			contexto.lineTo(x * anchoCelda, altoCalendario);
 			contexto.stroke;
 			contexto.strokeStyle = "#cc0000";
 
@@ -224,7 +240,7 @@ if (x > 0) {
 	contexto.fillStyle="green";
 	contexto.font = "12px sans-serif";	
 	contexto.textBaseline = "middle";
-	contexto.fillText(dias[x-1], (x*100)+20, 10);
+	contexto.fillText(dias[x-1], (x*anchoCelda)+altoCelda, 10);
 //	contexto.closePath();
 //	alert("Día: "+dias[y-1]);
 			
@@ -235,17 +251,12 @@ if (y > 1) {
 	contexto.fillStyle="green";
 	contexto.font = "12px sans-serif";	
 	contexto.textBaseline = "middle";
-	contexto.fillText(horas[y-2], 30, (y*20)-10);
+	contexto.fillText(horas[y-2], 30, (y*altoCelda)-10);
 //	contexto.closePath();
 //	alert("Día: "+dias[y-1]);
 			
 }
 
-
-}
-
-
-function escribirHoras(){
 
 }
 
@@ -265,7 +276,7 @@ function definirCelda(x, y, ancho, alto){
 	contexto.fillStyle="white";
 	contexto.font = "12px sans-serif";	
 	contexto.textBaseline = "middle";
-	contexto.fillText(frase, x, (y + 20));
+	contexto.fillText(frase, x, (y + altoCelda));
 	contexto.closePath();
 	
 
